a9c4bb2eb2c6cb810d1dc0da7b0e4411153d768f
George Harrison <122561008+GLZX4@users.noreply.github.com>
2023-08-15 03:10:53 +0100

    Add files via upload

--- GRFM Web/BackgroundJS/randomMinecraft.js
+++ GRFM Web/BackgroundJS/randomMinecraft.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     const youtubeVideos = [
         'https://www.youtube.com/watch?v=zLkYYSfqkqw',
         'https://www.youtube.com/watch?v=G5ivDCpsLAs',
+        'https://www.youtube.com/watch?v=JC3uhLT24Bc',
     ];
 
     console.log('Random Minecraft video loaded');
